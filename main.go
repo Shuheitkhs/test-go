97b1139b6a4e6fa50ede4bf8186d24f0bc04b317
Stk <shuheitakahashi0822@gmail.com>
2024-12-19 09:25:31 +0900

    ✨ Todoの追加の完成

--- main.go
+++ main.go
@@ -2,50 +2,48 @@ package main
 
 import (
 	"html/template" // HTMLテンプレートを扱うためのパッケージ
-	"log"           // ログ出力用パッケージ
 	"net/http"      // HTTPサーバー機能を提供するパッケージ
 )
 
-// Todoリストを格納する変数（初期値は空のスライス＊JavaScriptの配列みたいなもの）
+// Todoリストを格納する空のスライス
 var todoList []string
 
-// "/todo" にアクセスされた時に実行されるハンドラ関数
+// "/todo" パスのハンドラ関数: Todoリストを表示
 func handleTodo(w http.ResponseWriter, r *http.Request) {
-	// "templates/todo.html" テンプレートファイルをパース（解析）してtに格納
-	t, err := template.ParseFiles("templates/todo.html")
-	if err != nil {
-		// テンプレートファイルの読み込みに失敗したらエラーログを出力し、処理を終了
-		log.Println("Error loading template:", err)
-		return
-	}
+    // templates/todo.htmlを解析してテンプレートを作成
+    t, _ := template.ParseFiles("templates/todo.html")
+    
+    // todoListの内容をテンプレートに埋め込み、クライアントに返す
+    t.Execute(w, todoList)
+}
 
-	// テンプレートを実行し、HTTPレスポンスにtodoListの内容を埋め込んで返す
-	// t.Executeは、第一引数にレスポンスライター、第二引数にデータ（todoList）を渡す
-	err = t.Execute(w, todoList)
-	if err != nil {
-		log.Println("Error executing template:", err)
-	}
+// "/add" パスのハンドラ関数: Todoリストに新しいタスクを追加
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+    // フォームデータを解析
+    r.ParseForm()
+    
+    // "todo"という名前のフォームの値を取得
+    todo := r.Form.Get("todo")
+    
+    // 取得したタスクをtodoListに追加
+    todoList = append(todoList, todo)
+    
+    // Todoリストのページを再表示
+    handleTodo(w, r)
 }
 
 func main() {
-	// ★ 固定のTodoリストを作成
-	// append関数は、スライス（配列のようなもの）に要素を追加する
-	// ここでは、todoListに3つのタスクを追加している
-	todoList = append(todoList, "顔を洗う", "朝食を食べる", "歯を磨く")
+    // ★ 静的ファイルのサーバー設定
+    // "/static/" というURLパスを "static" ディレクトリに対応付け
+    http.Handle("/static/",
+        http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	// ★ 静的ファイル（CSSやJS、画像など）を提供する設定
-	// FileServer: 指定されたディレクトリのファイルをクライアントに返す
-	// StripPrefix: URLの「/static/」部分を削除してディレクトリ内のファイルに対応付ける
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+    // ★ "/todo" パスにアクセスが来たら handleTodo 関数を実行
+    http.HandleFunc("/todo", handleTodo)
 
-	// ★ "/todo"パスにアクセスされた場合にhandleTodo関数を実行するよう設定
-	http.HandleFunc("/todo", handleTodo)
+    // ★ "/add" パスにアクセスが来たら handleAdd 関数を実行
+    http.HandleFunc("/add", handleAdd)
 
-	// ★ HTTPサーバーを起動し、ポート8080でリクエストを待ち受ける
-	// 第二引数のnilはデフォルトのマルチプレクサ（http.DefaultServeMux）を使うことを意味する
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		// サーバーの起動に失敗した場合にエラーログを出力して終了
-		log.Fatal("failed to start : ", err)
-	}
+    // ★ HTTPサーバーを起動し、ポート8080で待ち受け
+    http.ListenAndServe(":8080", nil)
 }
